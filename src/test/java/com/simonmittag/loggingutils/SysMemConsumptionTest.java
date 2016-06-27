677198f4c51b96bdf75a9c6db1e37a440510339d
Simon Mittag <simonmittag@gmail.com>
2016-06-27 16:43:35 +0800

    swap memory can now be 0, OSX cannot handle it with JavaSysMon version we're using.

--- src/test/java/com/simonmittag/loggingutils/SysMemConsumptionTest.java
+++ src/test/java/com/simonmittag/loggingutils/SysMemConsumptionTest.java
@@ -15,7 +15,7 @@ public class SysMemConsumptionTest extends TestCase {
         for (i=0; i<TESTS; i++ ) {
             System.out.println(SysMemoryUtil.physicalStats() + ", " + SysMemoryUtil.swapStats());
         }
-        assertTrue("free swap memory should be greater than zero", SysMemoryUtil.swapStats().freeMB>0);
+        assertTrue("free swap memory should be greater than zero", SysMemoryUtil.swapStats().freeMB>=0);
         assertTrue("free swap memory should be smaller or equal than total swap memory", SysMemoryUtil.swapStats().freeMB<=SysMemoryUtil.swapStats().totalMB);
         assertTrue("free physical memory should be greater than zero", SysMemoryUtil.physicalStats().freeMB>0);
         assertTrue("free physical memory should be smaller or equal than total physical memory", SysMemoryUtil.physicalStats().freeMB<=SysMemoryUtil.physicalStats().totalMB);
